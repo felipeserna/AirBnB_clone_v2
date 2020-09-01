eaaabae19c7e1ede5d75cad8a65eff9d28b66201
Felipe Serna <feserna86@gmail.com>
2020-09-01 00:54:18 +0000

    1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+Two routes
+"""
+
+
+from flask import Flask
+# create an instance of the Flask class
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_flask():
+    """displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    """displays HBNB"""
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
