93fdc0cdb96002f33d5605493a2b840ff7916263
Felipe Serna <feserna86@gmail.com>
2020-09-02 14:56:56 +0000

    task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,3 +75,7 @@ class DBStorage():
         Session = scoped_session(sessionmaker(bind=self.__engine,
                                               expire_on_commit=False))
         self.__session = Session()
+
+    def close(self):
+        """Public method that calls close() method"""
+        self.__session.close()
